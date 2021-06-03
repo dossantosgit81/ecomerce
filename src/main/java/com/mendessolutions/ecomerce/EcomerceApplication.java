73ae9da28ac805afcbed67154679fcd3dc9d2af6
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2021-06-03 16:16:53 -0300

    Instanciando objetos automaticamente para testes

--- src/main/java/com/mendessolutions/ecomerce/EcomerceApplication.java
+++ src/main/java/com/mendessolutions/ecomerce/EcomerceApplication.java
@@ -1,13 +1,33 @@
 package com.mendessolutions.ecomerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mendessolutions.ecomerce.domain.Categoria;
+import com.mendessolutions.ecomerce.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EcomerceApplication {
+public class EcomerceApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository cateRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		cateRepo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
