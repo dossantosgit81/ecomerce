95e2a38726196ea567bcd2b8243f0e0930e6a1c8
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2021-06-08 16:16:59 -0300

    Listando todas as categorias

--- src/main/java/com/mendessolutions/ecomerce/resources/CategoriaResource.java
+++ src/main/java/com/mendessolutions/ecomerce/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.mendessolutions.ecomerce.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mendessolutions.ecomerce.domain.Categoria;
+import com.mendessolutions.ecomerce.dto.CategoriaDTO;
 import com.mendessolutions.ecomerce.services.CategoriaService;
 
 @RestController
@@ -53,6 +56,13 @@ public class CategoriaResource {
 		
 		return ResponseEntity.noContent().build();
 
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = catService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
 	}
 
 }
--- src/main/java/com/mendessolutions/ecomerce/services/CategoriaService.java
+++ src/main/java/com/mendessolutions/ecomerce/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.mendessolutions.ecomerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +44,10 @@ public class CategoriaService {
 		}
 		
 	}
+	
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
+	}
+	
+	
 }
